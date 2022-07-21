22c1f81350d2cab2549d4b1283f4d571f52f9b53
Dave Friedman <davemfriedman@gmail.com>
2022-07-21 17:41:25 -0400

    first public commit

---
+++ todo/appcore/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class AppCoreConfig(AppConfig):
+    name = "todo.appcore"
+    verbose_name = _("Todo")
---
+++ todo/appcore/models.py
@@ -0,0 +1,18 @@
+from django.db.models import *
+
+from todo.users.models import User
+
+
+class Task(Model):
+    name = CharField(max_length=64)
+    description = TextField(max_length=480)
+
+    created_on = DateTimeField(auto_now_add=True)
+    due_on = DateTimeField(null=True, blank=True)
+    completed_on = DateTimeField(null=True, blank=True)
+    is_completed = BooleanField(default=False)
+
+    tasked_to = ForeignKey(User, on_delete=CASCADE)
+
+    def __str__(self):
+        return f"{self.tasked_to}'s task: {self.name}"
